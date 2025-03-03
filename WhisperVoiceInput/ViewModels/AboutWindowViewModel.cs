a278eb0ebb200396bb8088758030425282cf7170
Vladimir Rogozhin <vladimirrogozhin90@gmail.com>
2025-03-03 02:45:55 +0100

    feat: display application version in AboutWindow

--- WhisperVoiceInput/ViewModels/AboutWindowViewModel.cs
+++ WhisperVoiceInput/ViewModels/AboutWindowViewModel.cs
@@ -1,4 +1,5 @@
 using System.Reactive;
+using System.Reactive.Linq;
 using ReactiveUI;
 using Avalonia.Controls;
 using Avalonia.ReactiveUI;
@@ -9,10 +10,18 @@ namespace WhisperVoiceInput.ViewModels
     {
         private readonly Window _window;
 
+        private readonly ObservableAsPropertyHelper<string> _version;
+        public string Version => _version.Value;
+        
+
         public AboutWindowViewModel(Window window)
         {
             _window = window;
             CloseCommand = ReactiveCommand.Create(Close, null, AvaloniaScheduler.Instance);
+
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "Unknown";
+            _version = Observable.Return("Version: " + version)
+                .ToProperty(this, nameof(Version));
         }
 
         public ReactiveCommand<Unit, Unit> CloseCommand { get; }
